c9b0bf82efeb3de0f356fd7a5f2e95d2a3b76eff
Vyacheslav Efimov <slavaheads@gmail.com>
2025-09-14 21:24:52 +0200

    Small update

--- main.py
+++ main.py
@@ -1,27 +1,23 @@
-import os
 from mcp.server.fastmcp import FastMCP
 from pydantic import Field
-
-
-port = int(os.getenv("PORT", 3000))
 
 
 mcp = FastMCP(
     name="Hello MCP Server",
     host="0.0.0.0",
-    port=port,
+    port=3000,
     stateless_http=True,
     debug=False,
 )
 
 @mcp.tool(
     title="Welcome a user",
-    description="Welcome a user by returning a phrase",
+    description="Return a friendly welcome message for the user.",
 )
 def welcome(
     name: str = Field(description="Name of the user")
 ) -> str:
-    return f"Welcome {name} to this amazing MCP server!"
+    return f"Welcome {name} from this amazing application!"
 
 
 if __name__ == "__main__":
